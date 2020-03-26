a0df1adbd24152f0d2b842154a05eca898facbeb
LotteWong <LotteWong21@gmail.com>
2020-03-26 19:22:04 +0800

    以前缀作分组，支持分组嵌套

--- gingle/gingle.go
+++ gingle/gingle.go
@@ -9,13 +9,29 @@ type HandlerFunc func(*Context)
 
 // TODO: Handler 分离路由部件
 type Engine struct {
+	*RouterGroup
+	groups []*RouterGroup
 	router *router
 }
 
+// TODO: RouterGroup 分组路由嵌套
+type RouterGroup struct {
+	prefix      string
+	parent      *RouterGroup
+	middlewares []HandlerFunc
+	engine      *Engine
+}
+
 func New() *Engine {
-	return &Engine{
+	// TODO: 你中有我我中有你
+	engine := &Engine{
 		router: newRouter(),
 	}
+	engine.RouterGroup = &RouterGroup{
+		engine: engine,
+	}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // TODO: ListenAndServe 监听
@@ -29,18 +45,36 @@ func (engine *Engine) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	engine.router.handle(ctx)  // TODO: 关键转换
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("GET", pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	subgroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	// TODO: 无所谓先后的顺序
+	engine.groups = append(engine.groups, subgroup)
+	return subgroup
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("POST", pattern, handler)
+func (group *RouterGroup) register(method string, subpattern string, handler HandlerFunc) {
+	// TODO: 组合继承与匿名成员
+	pattern := group.prefix + subpattern
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) PUT(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("PUT", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.register("GET", pattern, handler)
 }
 
-func (engine *Engine) DELETE(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("DELETE", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.register("POST", pattern, handler)
+}
+
+func (group *RouterGroup) PUT(pattern string, handler HandlerFunc) {
+	group.register("PUT", pattern, handler)
+}
+
+func (group *RouterGroup) DELETE(pattern string, handler HandlerFunc) {
+	group.register("DELETE", pattern, handler)
 }
--- main.go
+++ main.go
@@ -8,30 +8,36 @@ import (
 func main() {
 	router := gingle.New()
 
-	router.GET("/testString", func(ctx *gingle.Context) {
-		ctx.String(http.StatusOK, "Message = %s\nPattern = %s\nMethod = %s\n", ctx.Query("msg"), ctx.Pattern, ctx.Method)
-	})
-
-	router.GET("/testString/:msg", func(ctx *gingle.Context) {
-		ctx.String(http.StatusOK, "Message = %s\nPattern = %s\nMethod = %s\n", ctx.Param("msg"), ctx.Pattern, ctx.Method)
-	})
-
-	router.POST("/testJSON", func(ctx *gingle.Context) {
-		ctx.JSON(http.StatusOK, gingle.H{
-			"username": ctx.PostForm("username"),
-			"password": ctx.PostForm("password"),
-		})
-	})
-
-	router.POST("/testJSON/*info", func(ctx *gingle.Context) {
-		ctx.JSON(http.StatusOK, gingle.H{
-			"info": ctx.Param("info"),
-		})
-	})
-
 	router.GET("/testHTML", func(ctx *gingle.Context) {
 		ctx.HTML(http.StatusOK, "<h1>Hello Gingle!</h1>")
 	})
+
+	testString := router.Group("/testString")
+	{
+		testString.GET("/", func(ctx *gingle.Context) {
+			ctx.String(http.StatusOK, "Message = %s\nPattern = %s\nMethod = %s\n", ctx.Query("msg"), ctx.Pattern, ctx.Method)
+		})
+
+		testString.GET("/:msg", func(ctx *gingle.Context) {
+			ctx.String(http.StatusOK, "Message = %s\nPattern = %s\nMethod = %s\n", ctx.Param("msg"), ctx.Pattern, ctx.Method)
+		})
+	}
+
+	testJSON := router.Group("/testJSON")
+	{
+		testJSON.POST("/", func(ctx *gingle.Context) {
+			ctx.JSON(http.StatusOK, gingle.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+
+		testJSON.POST("/*info", func(ctx *gingle.Context) {
+			ctx.JSON(http.StatusOK, gingle.H{
+				"info": ctx.Param("info"),
+			})
+		})
+	}
 
 	router.Run(":8080")
 }
